1eac9c17b3b89f7396a5a11e35a63fac6737a8e4
Karri_Aditya <adityakarri01@gmail.com>
2025-06-11 11:19:20 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,29 +5,20 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import BackgroundDesign from "./components/BackgroundDesign";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      {/* Notifications */}
       <Toaster />
       <Sonner />
-
-      {/* Routing */}
       <BrowserRouter>
-        {/* Global Background */}
-        <div className="relative min-h-screen bg-[linear-gradient(135deg,#0fffc1_0%,#22223b_50%,#7e0fff_100%)] animate-gradient-x overflow-hidden">
-          <BackgroundDesign />
-          
-          {/* Routes */}
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </div>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
